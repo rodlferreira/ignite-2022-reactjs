49683158115b8b54e39c904cf33333b432fcae71
rodlferreira2 <rodriggo.lferreira@icloud.com>
2022-08-29 23:22:06 -0300

    feat: projeto em desenvolvimento, já em prod pelo firebase

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/58087865?v=4" alt="rodlferreira" />
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/58087865?v=4" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,21 @@
 import styles from './Sidebar.module.css';
 import me from '../assets/me.jpg';
 import { FilePdf } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src={me} alt="Rodriggo Ferreira" />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/58087865?v=4" alt="rodlferreira" />
+                <Avatar src={'https://avatars.githubusercontent.com/u/58087865?v=4'} />
+                {/* <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/58087865?v=4" alt="rodlferreira" /> */}
                 <strong>Rodriggo Ferreira</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
-                    <FilePdf size={20}/>
+                    <FilePdf size={20} />
                     Visualizar o CV
                 </a>
             </footer>
